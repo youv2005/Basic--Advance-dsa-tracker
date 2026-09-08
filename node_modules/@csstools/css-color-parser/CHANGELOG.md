9715dff985f49e2f6308bee3a51f934a1cd1e37c
youv2005 <youv0606@gmail.com>
2026-09-08 18:51:54 +0530

    Add Vercel Analytics

---
+++ node_modules/@csstools/css-color-parser/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changes to CSS Color Parser
+
+### 4.2.1
+
+_August 25, 2026_
+
+- Preserve powerless hue in `color-mix()` when the input color space equals the interpolation color space, matching relative color syntax.
+
+[Full CHANGELOG](https://github.com/csstools/postcss-plugins/tree/main/packages/css-color-parser/CHANGELOG.md)
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -12,5 +13,6 @@ if (import.meta.env.PROD && "serviceWorker" in navigator) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
